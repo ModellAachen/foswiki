69beeee46efddfd98d511b8c9d4edfc889d3f10d
Jan Krüger <krueger@modell-aachen.de>
2012-05-04 11:58:49 +0200

    CKEditorPlugin: diverse: verbesserte Pfadbehandlung für Dateiuploads
    Ändere diverse Plugins (und den REST-Upload-Handler von WysiwygPlugin)
    so ab, dass Dateiupload-Links intern mit einem %ATTACHURL%-Präfix
    gespeichert werden; verstecke dieses Präfix in der Oberfläche.
    
    Entferne außerdem weiteren unnötigen Code aus dem Dateiupload-Dialog und
    aktualisiere die Sprachdateien. Einige entfernte Sprachstrings wurden
    gar nicht verwendet; außerdem hat sich die Beschriftung des
    Dateiupload-Linkfelds etwas geändert ("Dateiname" -> "Datei"), um
    weniger stark zu suggerieren, dass es bei dem Feld nur um die
    Namenskomponente des Dateipfads geht. Das reduziert hoffentlich
    Verwirrung.

--- CKEditorPlugin/pub/System/CKEditorPlugin/ckeditor/_source/plugins/qwiki/dialogs/document.js
+++ CKEditorPlugin/pub/System/CKEditorPlugin/ckeditor/_source/plugins/qwiki/dialogs/document.js
@@ -5,46 +5,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.dialog.add( 'document', function( editor )
 {
-	// Loads the parameters in a selected link to the link dialog fields.
-	var urlRegex = /^((?:http|https|ftp|news):\/\/)?(.*)$/;
-
-	var parseLink = function( editor, element )
-	{
-		var href = element ? ( element.getAttribute( 'data-cke-saved-href' ) || element.getAttribute( 'href' ) ) : '',
-			urlMatch,
-			retval = {};
-
-		// urlRegex matches empty strings, so need to check for href as well.
-		if (  href && ( urlMatch = href.match( urlRegex ) ) )
-		{
-			retval.type = 'url';
-			retval.url = {};
-			retval.url.protocol = urlMatch[1];
-			retval.url.url = urlMatch[2];
-		}
-		else
-			retval.type = 'url';
-
-		// Record down the selected element in the dialog.
-		this._.selectedElement = element;
-
-		return retval;
-	};
-
-	var setupParams = function( page, data )
-	{
-		if ( data[page] )
-			this.setValue( data[page][this.id] || '' );
-	};
-
-	var commitParams = function( page, data )
-	{
-		if ( !data[page] )
-			data[page] = {};
-
-		data[page][this.id] = this.getValue() || '';
-	};
-
 	return {
 		title : editor.lang.qwikidocument.title,
 		minWidth : 350,
@@ -85,26 +45,12 @@ CKEDITOR.dialog.add( 'document', function( editor )
 											var func = CKEDITOR.dialog.validate.notEmpty( editor.lang.qwikidocument.noUrl );
 											return func.apply( this );
 										},
-										setup : function( data )
-										{
-											if ( data.url )
-												this.setValue( data.url.url );
-
-										},
 										commit : function( data )
 										{
-											if ( !data.url )
-												data.url = {};
-
-											data.url.url = this.getValue();
+											data.url = this.getValue();
 										}
 									}
-								],
-								setup : function( data )
-								{
-									if ( !this.getDialog().getContentElement( 'info', 'linkType' ) )
-										this.getElement().show();
-								}
+								]
 							},
 							{
 								type : 'button',
@@ -119,7 +65,6 @@ CKEDITOR.dialog.add( 'document', function( editor )
 			},
 			{
 				id : 'upload',
-				hidden : true,
 				ckefilebrowser : 'uploadButton',
 				label : editor.lang.image.upload,
 				elements :
@@ -132,7 +77,7 @@ CKEDITOR.dialog.add( 'document', function( editor )
 						size : 26,
 						onChange : function()
 						{
-							// Modac : Hier sollte eine onChange Funktionalität hin
+							this.getDialog().getContentElement('upload', 'filename').onFocus();
 						}
 					},
 					{
@@ -154,17 +99,6 @@ CKEDITOR.dialog.add( 'document', function( editor )
 						}
 					},
 					{
-						type : 'select',
-						hidden : 'true',
-						id : 'uploadtarget',
-						label : editor.lang.qwikidocument.uploadOptions,
-						'default' : 'local',
-						items :
-							[
-								[ 'Attach to this topic' , 'local']
-							]
-					},
-					{
 						type : 'fileButton',
 						id : 'uploadButton',
 						label : editor.lang.common.uploadSubmit,
@@ -172,41 +106,14 @@ CKEDITOR.dialog.add( 'document', function( editor )
 							{
 						 		action : 'UploadPOST', //required
 						 		target : 'info:url', //required
+								params : { relativepath: 1 },
 						 		url : FoswikiCKE.getFoswikiVar("SCRIPTURL") + '/rest/WysiwygPlugin/upload'
 						 	},
 						'for' : [ 'upload', 'filepath']
 					}
-				],
-				setup : function()
-				{
-					//alert("muuusso");
-				}
+				]
 			}
 		],
-		onShow : function()
-		{
-			var editor = this.getParentEditor(),
-				selection = editor.getSelection(),
-				ranges = selection.getRanges(),
-				element = null,
-				me = this;
-			// Fill in all the relevant fields if there's already one link selected.
-			if ( ranges.length == 1 )
-			{
-
-				var rangeRoot = ranges[0].getCommonAncestor( true );
-				element = rangeRoot.getAscendant( 'a', true );
-				if ( element && element.getAttribute( 'href' ) )
-				{
-					selection.selectElement( element );
-				}
-				else
-					element = null;
-			}
-
-			this.setupContent( parseLink.apply( this, [ editor, element ] ) );
-
-		},
 		onOk : function()
 		{
 			var attributes = { href : 'javascript:void(0)/*' + CKEDITOR.tools.getNextNumber() + '*/' },
@@ -216,9 +123,8 @@ CKEDITOR.dialog.add( 'document', function( editor )
 			this.commitContent( data );
 
 			// Compose the URL.
-			var protocol = ( data.url && data.url.protocol != undefined ) ? data.url.protocol : 'http://',
-				url = ( data.url && data.url.url ) || '';
-			attributes['data-cke-saved-href'] = ( url.indexOf( '/' ) === 0 ) ? url : protocol + url;
+			var url = data.url || '';
+			attributes['data-cke-saved-href'] = "%ATTACHURL%/" + url;
 
 			if ( !this._.selectedElement )
 			{
@@ -227,7 +133,7 @@ CKEDITOR.dialog.add( 'document', function( editor )
 					ranges = selection.getRanges();
 				if ( ranges.length == 1 && ranges[0].collapsed )
 				{
-					var text = new CKEDITOR.dom.text( attributes['data-cke-saved-href'], editor.document );
+					var text = new CKEDITOR.dom.text( url, editor.document );
 					ranges[0].insertNode( text );
 					ranges[0].selectNodeContents( text );
 					selection.selectRanges( ranges );
--- CKEditorPlugin/pub/System/CKEditorPlugin/ckeditor/_source/plugins/qwikifilebrowser/plugin.js
+++ CKEditorPlugin/pub/System/CKEditorPlugin/ckeditor/_source/plugins/qwikifilebrowser/plugin.js
@@ -144,16 +144,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 	    //Parameter mit den Auswahlelementen erweitern
 	    params.filename = dialog.getContentElement( this[ 'for' ][ 0 ], 'filename' ).getInputElement().$.value;
 	    
-	    //Web und Topic auslesen
-	    if (dialog.getContentElement( this[ 'for' ][ 0 ], 'uploadtarget' ).getInputElement().$.value == 'local')
-	    {
-	    	params.topic = utf8_decode(FoswikiCKE.getTopicPath());
-	    	
-	    }
-	    else
-	    {
-	    	params.topic = "Main.WebAttachments";
-	    }
+		params.topic = utf8_decode(FoswikiCKE.getTopicPath());
 	    
 	    for ( var i in params )
 	    {
