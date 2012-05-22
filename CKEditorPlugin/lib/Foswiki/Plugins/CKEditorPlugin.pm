00ff83f94565806bcdb5dca75980e50795294284
Jan Krüger <krueger@modell-aachen.de>
2012-05-22 11:07:08 +0200

    CKEditorPlugin: Release 1.9.3
    Enthaltene Änderungen:
    
    Jan Krüger (1):
          CKEditorPlugin: floatpanel: neue Lösung für Ausrichtung am rechten Rand

--- CKEditorPlugin/lib/Foswiki/Plugins/CKEditorPlugin.pm
+++ CKEditorPlugin/lib/Foswiki/Plugins/CKEditorPlugin.pm
@@ -8,7 +8,7 @@ use warnings;
 use Assert;
 
 our $VERSION           = '$Rev$';
-our $RELEASE           = '1.9.2';
+our $RELEASE           = '1.9.3';
 our $SHORTDESCRIPTION  = 'Integration of the CKEditor WYSIWYG Editor';
 our $NO_PREFS_IN_TOPIC = 1;
 
