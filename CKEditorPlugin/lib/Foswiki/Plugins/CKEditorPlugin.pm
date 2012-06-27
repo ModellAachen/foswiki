25a7848840e25c10ea60c5ef55741280b7d4a2b4
Jan Krüger <krueger@modell-aachen.de>
2012-06-27 13:13:33 +0200

    CKEditorPlugin: Release 1.9.15

--- CKEditorPlugin/lib/Foswiki/Plugins/CKEditorPlugin.pm
+++ CKEditorPlugin/lib/Foswiki/Plugins/CKEditorPlugin.pm
@@ -8,7 +8,7 @@ use warnings;
 use Assert;
 
 our $VERSION           = '$Rev$';
-our $RELEASE           = '1.9.14';
+our $RELEASE           = '1.9.15';
 our $SHORTDESCRIPTION  = 'Integration of the CKEditor WYSIWYG Editor';
 our $NO_PREFS_IN_TOPIC = 1;
 
