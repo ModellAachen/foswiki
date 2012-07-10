d893b9cd54884777a7c0f5ed0f3121783da18713
Jan Krüger <krueger@modell-aachen.de>
2012-07-10 11:37:56 +0200

    CKEditorPlugin: qwikifilebrowser: UTF-8 im Topicnamen nicht zerhacken
    Hier entstanden äußerst skurrile neue Topics beim Hochladen von
    Attachments für Topics mit einem Zeichen über U+007F. Entferne die
    merkwürdige utf8_decode-Funktion in qwikifilebrowser, ohne die alles
    wunderbar funktioniert.

--- CKEditorPlugin/pub/System/CKEditorPlugin/ckeditor/_source/plugins/qwikifilebrowser/plugin.js
+++ CKEditorPlugin/pub/System/CKEditorPlugin/ckeditor/_source/plugins/qwikifilebrowser/plugin.js
@@ -144,7 +144,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 	    //Parameter mit den Auswahlelementen erweitern
 	    params.filename = dialog.getContentElement( this[ 'for' ][ 0 ], 'filename' ).getInputElement().$.value;
 	    
-		params.topic = utf8_decode(FoswikiCKE.getTopicPath());
+	    params.topic = FoswikiCKE.getTopicPath();
 	    
 	    for ( var i in params )
 	    {
@@ -160,37 +160,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 	    }
 	    return true;
 	}
-	
-	function utf8_decode (utftext) {
-		var string = "";
-		var i = 0;
-		var c = c1 = c2 = 0;
- 
-		while ( i < utftext.length ) {
- 
-			c = utftext.charCodeAt(i);
- 
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			}
-			else if((c > 191) && (c < 224)) {
-				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-				i += 2;
-			}
-			else {
-				c2 = utftext.charCodeAt(i+1);
-				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
- 
-		}
- 
-		return string;
-	}
-	
 	
 	/**
 	 * Adds (additional) arguments to given url.
